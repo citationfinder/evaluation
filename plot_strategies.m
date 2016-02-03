153b663e48f06c8930658ba0037261d40ac2a4d3
Xennis <code@xennis.org>
2016-02-03 18:20:38 +0100

    improve code

--- plot_strategies.m
+++ plot_strategies.m
@@ -1,12 +1,15 @@
-author_strategy = csvread('data/author_strategy/332714384.csv');
-journal_strategy = csvread('data/journal_strategy/332714384.csv');
+id = '332714384';
+strategies = {'author_strategy', 'journal_strategy', 'conference_strategy'};
 
 figure
     hold on
-    plot(author_strategy(:,1), author_strategy(:,2), '.-')
-    plot(journal_strategy(:,1), journal_strategy(:,2), '.-')    
+    for i=1:length(strategies)
+        data = csvread(['data/', strategies{i}, '/', id,'.csv']);
+        plot(data(:,1), data(:,2), '.-')
+    end
     hold off
     grid on
+    title(['Strategies comparison for user ', id])
     xlabel('inspected publications')
     ylabel('citations')
-    legend('author strategy', 'journal strategy')
+    legend(strategies)
