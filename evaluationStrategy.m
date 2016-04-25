ea36c619dbcf03870d0ffe9360956d4d144ba33f
Xennis <code@xennis.org>
2016-04-25 14:05:03 +0200

    update evaluation strategy

---
+++ evaluationStrategy.m
@@ -0,0 +1,43 @@
+function evaluationStrategy(name, strategies, do_plot, strategies_legend)
+
+    if nargin < 2
+        error('Not enough input arguments.')
+    elseif nargin < 3
+        do_plot = false;
+    end
+    if nargin < 4
+        strategies_legend = strategies;
+    end
+    
+    evaluation_dir = fullfile('evaluation_data', name);
+    if ~isdir(evaluation_dir)
+       error('Evaluation files does not exists')
+    end
+    
+
+    fprintf('%s\t%s\t%s\n','effectiveness', 'efficiency', 'strategy')
+    for i = 1:length(strategies)
+        filename = fullfile(evaluation_dir, strcat('meta_', strategies{i}, '.csv'));
+        
+        % author_ids, author_num_citations, author_num_publications,
+        % num_inspected_publications, num_citations
+        C = readLog(filename, '%u32 %f %f %f %f', 1);
+        plotStrategy(strategies{i}, C{2}, C{4}, C{5}, do_plot);         
+    end
+
+    if do_plot
+        figure(1)
+            grid on
+            legend(strategies_legend)
+            %title('effectiveness')
+            xlabel('Messung')
+            ylabel('Effektivität')
+        figure(2)
+            grid on
+            legend(strategies_legend)
+            %title('efficiency')
+            xlabel('Messung')
+            ylabel('Effizienz')
+    end
+
+end
--- plotStrategy.m
+++ plotStrategy.m
@@ -1,32 +1,32 @@
-function [effectiveness_mean, efficiency_mean] = plotStrategy(strategy_name, author_num_citations, num_inspected_publications, num_citations, plot)
+function [effectiveness_mean, efficiency_mean] = plotStrategy(strategy_name, author_num_citations, num_inspected_publications, num_citations, do_plot)
 
     if nargin < 4
         error('Not enough input arguments.')
     elseif nargin < 5
-        plot = false;
+        do_plot = false;
     end
 
-            % remove zeros
-            idx = find(author_num_citations==0);
-            %warning('Ignore %f', idx)
-            author_num_citations(idx) = [];
-            num_inspected_publications(idx) = [];
-            num_citations(idx) = [];
+    % remove zeros
+    idx = find(author_num_citations==0);
+    %warning('Ignore %f', idx)
+    author_num_citations(idx) = [];
+    num_inspected_publications(idx) = [];
+    num_citations(idx) = [];
             
-            effectiveness = num_citations./author_num_citations;
-            %effectiveness(isnan(effectiveness)) = 0;
-            efficiency = num_citations./num_inspected_publications;
-            %efficiency(isnan(efficiency)) = 0;
+    effectiveness = num_citations./author_num_citations;
+    %effectiveness(isnan(effectiveness)) = 0;
+    efficiency = num_citations./num_inspected_publications;
+    %efficiency(isnan(efficiency)) = 0;
 
-            effectiveness_mean = nanmean(effectiveness);
-            efficiency_mean = nanmean(efficiency);
+    effectiveness_mean = nanmean(effectiveness);
+    efficiency_mean = nanmean(efficiency);
 
-            fprintf('%f\t%f\t%s\n', effectiveness_mean, efficiency_mean, strategy_name)
+    fprintf('%f\t%f\t%s\n', effectiveness_mean, efficiency_mean, strategy_name)
             
-    if plot
-            x_axis = 1:1:length(author_num_citations);
-            %{
-            figure(2+i)
+    if do_plot
+        x_axis = 1:length(author_num_citations);
+        %{
+        figure(2+i)
                 hold on
                 
                 [ax, h1, h2] = plotyy(x_axis, effectiveness, x_axis, efficiency, 'semilogy','plot');
@@ -39,17 +39,17 @@ function [effectiveness_mean, efficiency_mean] = plotStrategy(strategy_name, aut
                 xlabel(ax(1),'Measurement')
                 ylabel(ax(1),'Effectiveness #Citations / #Total Citations')
                 ylabel(ax(2),'Efficiency #Citations / #Inspected Publications')
-            %}
-            figure(1)
-                hold on
-                plot(x_axis, effectiveness)
-                %plot(get(gca,'xlim'), [effectiveness_mean effectiveness_mean])
-                hold off
-            figure(2)
-                hold on
-                plot(x_axis, efficiency)
-                %plot(get(gca,'xlim'), [efficiency_mean efficiency_mean])
-                hold off
+        %}
+        figure(1)
+            hold on
+            plot(x_axis, effectiveness)
+            %plot(get(gca,'xlim'), [effectiveness_mean effectiveness_mean])
+            hold off
+        figure(2)
+            hold on
+            plot(x_axis, efficiency)
+            %plot(get(gca,'xlim'), [efficiency_mean efficiency_mean])
+            hold off
     end
 
 end
